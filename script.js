3d443d65377da9c4421e1bc590c7992e27a2d4fe
Marcelo Malacalza <malacalzamarcelo@gmail.com>
2024-10-22 08:17:46 -0300

    primitives

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
 "use strict";
-console.log('HOLA MUNDO, estamos ready!');
+let studiedJavascript = true;
+if (studiedJavascript) {
+    console.log("You can still learn more about Typescript");
+}
+else {
+    console.log("You should learn Javascript first");
+}
+let interMiami = 11;
+let fcDallas = 11;
+let messi = 10;
+let messiPlay = true;
+function play(team1, team2, messiPlay) {
+    let reason = '';
+    if (messiPlay) {
+        team1 += messi;
+        reason = 'because Messi is playing';
+    }
+    if (team1 > team2)
+        console.log('The winner is Inter Miami' + reason);
+    if (team1 < team2)
+        console.log('The winner is FC Dallas');
+    if (team1 === team2)
+        console.log('It is a draw');
+}
+play(interMiami, fcDallas, messiPlay);
+let disney;
+disney = 'StarWars and Marvel';
+console.log(disney);
+disney = 15000000000;
+console.log(disney);
+disney = true;
+console.log(disney);
--- script.ts
+++ script.ts
@@ -1,1 +1,35 @@
-console.log('HOLA MUNDO, estamos ready!')
+let studiedJavascript: boolean = true;
+
+if(studiedJavascript){
+    console.log("You can still learn more about Typescript");
+} else {
+    console.log("You should learn Javascript first");
+}
+
+let interMiami: number = 11;
+let fcDallas: number = 11;
+let messi: number = 10;
+let messiPlay: boolean = true;
+
+function play(team1: number, team2: number, messiPlay: boolean): void {
+    let reason: string = ''
+    if(messiPlay) {
+        team1 += messi;
+        reason = 'because Messi is playing';
+    }
+    if(team1 > team2) console.log('The winner is Inter Miami' + reason);
+    if(team1 < team2) console.log('The winner is FC Dallas');
+    if(team1 === team2) console.log('It is a draw');
+}
+
+play(interMiami, fcDallas, messiPlay);
+
+let disney: any;
+disney = 'StarWars and Marvel';
+console.log(disney);
+
+disney = 15000000000
+console.log(disney);
+
+disney = true;
+console.log(disney);
